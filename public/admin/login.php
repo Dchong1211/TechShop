fbe99cd0d9fd09210957a1e1f6fd56fa97930767
minhhungnt123 <nguyenminhhung165096@gmail.com>
2025-11-10 22:57:15 +0700

    Push by minhhungnt123 on Mon 11/10/2025 22:57:15.49

--- public/admin/login.php
+++ public/admin/login.php
@@ -3,30 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Đăng Nhập Admin</title>
-    <link rel="stylesheet" href="/public/assets/css/cssAdmin/login.css">
+    <title>Đăng Nhập</title>
+    <link rel="stylesheet" href="/public/assets/css/cssAdmin/login.css"> 
 </head>
 <body>
 
-    <div class="login-container">
-        <div class="login-box">
-            <h2>Đăng Nhập Quản Trị Viên</h2>
-            <form action="index.php" method="POST">
-                <div class="input-group">
-                    <label for="username">Tên người dùng / Email</label>
-                    <input type="text" id="username" name="username" required>
-                </div>
-                
-                <div class="input-group">
-                    <label for="password">Mật khẩu</label>
-                    <input type="password" id="password" name="password" required>
-                </div>
-                
-                <button type="submit" class="login-button">Đăng Nhập</button>
-            </form>
-            
-            <p class="forgot-password"><a href="#">Quên mật khẩu?</a></p>
+    <div class="split-page-container">
+        
+        <div class="techshop-slogan">
+            <h1>TechShop</h1>
         </div>
+
+        <div class="login-container">
+            <div class="login-box">
+                <h2>Đăng Nhập Quản Trị Viên</h2>
+                <form action="index.php" method="POST">
+                    <div class="input-group">
+                        <label for="username">Tên người dùng / Email</label>
+                        <input type="text" id="username" name="username" required>
+                    </div>
+                    
+                    <div class="input-group">
+                        <label for="password">Mật khẩu</label>
+                        <input type="password" id="password" name="password" required>
+                    </div>
+                    
+                    <button type="submit" class="login-button">Đăng Nhập</button>
+                </form>
+                
+                <p class="forgot-password"><a href="#">Quên mật khẩu?</a></p>
+            </div>
+        </div>
+        
     </div>
 
 </body>
