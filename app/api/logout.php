32fa84268eac49bec4c85c699d697957800ca8b1
Dchong1211 <vdinhtrong580@gmail.com>
2025-11-14 09:32:17 +0700

    Push by Dchong1211 on 14/11/25  9:32:17.92

--- app/api/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-Type: application/json");
-require_once __DIR__ . '/../controllers/UserController.php';
-
-$controller = new UserController();
-$response = $controller->logout();
-echo json_encode($response);
-?>
