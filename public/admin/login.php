6ae3efda6b8726b43bc0af2a4dd99531ee88347e
minhhungnt123 <nguyenminhhung165096@gmail.com>
2025-11-18 15:47:40 +0700

    Push by minhhungnt123 on Tue 11/18/2025 15:47:39.97

--- public/admin/login.php
+++ public/admin/login.php
@@ -17,20 +17,21 @@
         <div class="login-container">
             <div class="login-box"> 
                 <h2>Đăng Nhập</h2>
-                <form action="login.php" method="POST">
-                    <div class="input-group">
-                        <label for="username">Tên người dùng / Email</label>
-                        <input type="text" id="username" name="username" required>
-                    </div>
-                    
-                    <div class="input-group">
-                        <label for="password">Mật khẩu</label>
-                        <input type="password" id="password" name="password" required>
-                    </div>
-                    
-                    <button type="submit" class="login-button">Đăng Nhập</button>
-                </form>
-                
+                    <form action="/login" method="POST">
+                        <input type="hidden" name="csrf_token" value="<?php echo $csrf_token; ?>"> 
+
+                        <div class="input-group">
+                            <label for="username">Tên người dùng / Email</label>
+                            <input type="text" id="username" name="email" required> </div>
+
+                        <div class="input-group">
+                            <label for="password">Mật khẩu</label>
+                            <input type="password" id="password" name="password" required>
+                        </div>
+
+                        <button type="submit" class="login-button">Đăng Nhập</button>
+                    </form>
+                                    
                 <div class="function">
                     <p class="forgot-password"><a href="/public/admin/forgot_password.php">Quên mật khẩu?</a></p>
                     <p class="register"><a href="/public/admin/register.php">Đăng kí</a></p>
