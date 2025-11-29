e946ecd7e0a47580dc292d498c6210c2287ded4d
Dchong1211 <vdinhtrong580@gmail.com>
2025-11-29 22:13:43 +0700

    Auto push by Dchong1211 at 29/11/25_22:13:43.16

--- auto_push.bat
+++ auto_push.bat
@@ -1,6 +1,24 @@
 @echo off
 cd /d C:\xampp\htdocs\TechShop
 
+:: ===== BACKUP SQL TỰ ĐỘNG =====
+echo Dang backup database...
+
+:: Đường dẫn mysqldump
+set mysqldump_path=C:\xampp\mysql\bin\mysqldump.exe
+
+:: Tên database
+set dbname=techshop
+
+:: File output
+set backup_file=C:\xampp\htdocs\TechShop\database\techshop.sql
+
+"%mysqldump_path%" -u root %dbname% > "%backup_file%"
+
+echo Backup SQL hoan tat: %backup_file%
+echo ===============================
+
+:: ===== AUTO PUSH GIT =====
 for /f "delims=" %%a in ('git config user.name') do set username=%%a
 
 set datetime=%date%_%time%
@@ -9,5 +27,5 @@ git add .
 git commit -m "Auto push by %username% at %datetime%"
 git pull origin main --rebase
 git push origin main
+
 echo     PUSH COMPLETED!
-pause
