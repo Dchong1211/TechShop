ecd2db436fba24b93a2fd53ee8a375e05815e505
Dchong1211 <vdinhtrong580@gmail.com>
2025-12-02 23:33:06 +0700

    Auto push by Dchong1211 at 02/12/25_23:33:06.02

--- public/admin/orders.php
+++ public/admin/orders.php
@@ -10,6 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quản lý Đơn hàng</title>
+    <meta name="csrf-token" content="<?= $csrf ?>">
     <link rel="stylesheet" href="../assets/css/cssAdmin/admin_style.css"> 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 </head>
